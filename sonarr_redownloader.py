d0923a1b760104df9cfb89bcab4e03571d54cf2c
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-06-30 01:08:08 -0700

    better docstring

--- sonarr_redownloader.py
+++ sonarr_redownloader.py
@@ -8,7 +8,7 @@ MAX_CONNECTION_RETRIES = 10 # Number of attempts
 CONNECTION_RETRY_TIMEOUT = 10 # Time in seconds
 
 def content_redownloader():
-    """Queries sonarr to upgrade content that match given parameters"""
+    """Queries Sonarr to upgrade content that match the user's given parameters"""
     # Sonarr configuration values
     print("\n  ** ex) http://192.168.86.20:8989")
     sonarr_url = str(input("Sonarr URL: "))
